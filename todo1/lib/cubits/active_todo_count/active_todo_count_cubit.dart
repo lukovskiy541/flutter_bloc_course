4e368faa1dd17046736c465b5b19c8334401c959
Max Lukovskiy <ragekrimson@gmail.com>
2024-11-19 20:49:45 +0200

    first commit

---
+++ todo1/lib/cubits/active_todo_count/active_todo_count_cubit.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:todo1/cubits/todo_list/todo_list_cubit.dart';
+import 'package:todo1/models/todo_model.dart';
+
+part 'active_todo_count_state.dart';
+
+class ActiveTodoCountCubit extends Cubit<ActiveTodoCountState> {
+  final TodoListCubit todoListCubit;
+  late final StreamSubscription todoListSubscription;
+  ActiveTodoCountCubit({required this.todoListCubit})
+      : super(ActiveTodoCountState.initial()) {
+    todoListSubscription =
+        todoListCubit.stream.listen((TodoListState todoListState) {
+      print(todoListState);
+
+      final currentActiveTodoCount = todoListState.todos
+          .where((Todo todo) => !todo.completed)
+          .toList()
+          .length;
+      emit(state.copyWith(todo_count: currentActiveTodoCount));
+    });
+  }
+
+  @override
+  Future<void> close() {
+    todoListSubscription.cancel();
+    return super.close();
+  }
+}
